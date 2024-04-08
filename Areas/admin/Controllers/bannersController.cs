6b1d5a3d0becba6e784399f7919f2c6aa2536b50
CookiesDan220603 <huytruong0939726205@gmail.com>
2024-04-08 19:44:17 +0700

    Push admin area, 6 webpage, controller

---
+++ Areas/admin/Controllers/bannersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProjectDotNet.Areas.admin.Controllers
+{
+    public class bannersController : Controller
+    {
+        // GET: admin/banners
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
